b03991b574a882236fc720e35c77747e687cc8b0
buzzdecafe <m_hur@yahoo.com>
2015-01-01 13:42:56 -0500

    improve map test coverage

--- test/map.js
+++ test/map.js
@@ -6,6 +6,12 @@ var R = require('..');
 describe('map', function() {
     var times2 = function(x) {return x * 2;};
     var add1 = function(x) {return x + 1;};
+    var dec = function(x) { return x - 1; };
+    var listxf = {
+        init: function() { return []; },
+        step: function(acc, x) { return acc.concat([x]); },
+        result: function(x) { return x; }
+    };
 
     it('maps simple functions over arrays', function() {
         assert.deepEqual(R.map(times2, [1, 2, 3, 4]), [2, 4, 6, 8]);
@@ -17,14 +23,24 @@ describe('map', function() {
     });
 
     it('dispatches to transformer objects', function() {
-        var obj = {
-            step: function(acc, x) { return acc.concat([x]); },
-            result: function(x) { return x; }
-        };
-        assert.deepEqual(R.map(add1, obj), {
+        assert.deepEqual(R.map(add1, listxf), {
             f: add1,
-            xf: obj
+            xf: listxf
         });
+    });
+
+    it('composes', function() {
+        var mdouble = R.map(times2);
+        var mdec = R.map(dec);
+        assert.deepEqual(mdec(mdouble([10, 20, 30])), [19, 39, 59]);
+    });
+
+    it('can compose transducer-style', function() {
+        var mdouble = R.map(times2);
+        var mdec = R.map(dec);
+        var xcomp = mdec(mdouble(listxf));
+        assert.deepEqual(xcomp.xf, {xf: listxf, f: times2});
+        assert.strictEqual(xcomp.f, dec);
     });
 
     it('is automatically curried', function() {
